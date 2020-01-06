326f95e58e2feacf7f3196728b606d8f7823031a
TobiOladoja <tbfullstack@gmail.com>
2020-01-06 09:43:17 +0000

    Nested ProjectSummary in ProjectList, to make code more efficient so can get the block of code through just calling ProjectSummary

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,10 +7,12 @@ export default class Dashboard extends Component {
     return (
       <div className='dashboard container'>
         <div className='row'>
-          <div className='col s12 m6'></div>
-          <ProjectList />
-          <div className='col s12 m5 offset-m1'></div>
-          <Notifications />
+          <div className='col s12 m6'>
+            <ProjectList />
+          </div>
+          <div className='col s12 m5 offset-m1'>
+            <Notifications />
+          </div>
         </div>
       </div>
     );
