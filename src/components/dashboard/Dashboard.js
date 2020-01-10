dc182f07f0b8258fef5576e7a96ea04d81f6084b
TobiOladoja <tbfullstack@gmail.com>
2020-01-10 18:21:30 +0000

    Connected application to redux store and grabbing data in our dashboard from that store and passing that down to project list and summary component then outputting it to the DOM, this is just dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux'; // Connecting dashboard component with the redux store.
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   render() {
+    // console.log(this.props);
+    const { projects } = this.props; // Pass this to the Project Lists
     return (
       <div className='dashboard container'>
         <div className='row'>
           <div className='col s12 m6'>
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className='col s12 m5 offset-m1'>
             <Notifications />
@@ -18,3 +21,12 @@ export default class Dashboard extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  // pass this function to connect
+  return {
+    projects: state.project.projects
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
